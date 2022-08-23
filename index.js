8367f343e5ef5e322a9411c7cd8f420e2f5e94ac
sAm2097 <samanugandula1518@gmail.com>
2022-08-23 15:58:21 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const buttonElement=document.querySelector(".btn")
+
+buttonElement.addEventListener("mouseover",(event)=>{
+    // console.log(e.pageY,e.pageX)
+    const x=event.pageX-buttonElement.offsetLeft;
+    const y=event.pageY-buttonElement.offsetTop;
+
+    buttonElement.style.setProperty("--xPos",x+ "px")
+    buttonElement.style.setProperty("--yPos",y+ "px")
+
+
+})
